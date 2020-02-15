5827d44f222d2cf124c7ec28d048c8492ebb161a
Basar Buyukkahraman <basarbk@yahoo.com>
2020-02-16 00:08:36 +0300

    0202-layout

--- frontend/src/UserSignupPage.js
+++ frontend/src/UserSignupPage.js
@@ -2,7 +2,28 @@ import React from 'react';
 
 class UserSignupPage extends React.Component {
   render() {
-    return <h1>Sign Up</h1>;
+    return (
+      <form>
+        <h1>Sign Up</h1>
+        <div>
+          <label>Username</label>
+          <input />
+        </div>
+        <div>
+          <label>Display Name</label>
+          <input />
+        </div>
+        <div>
+          <label>Password</label>
+          <input type="password" />
+        </div>
+        <div>
+          <label>Password Repeat</label>
+          <input type="password" />
+        </div>
+        <button>Sign Up</button>
+      </form>
+    );
   }
 }
 
