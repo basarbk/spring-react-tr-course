1c5e805396e25c4d0ffcdb7a1737bb774aa08e67
Basar Buyukkahraman <basarbk@yahoo.com>
2020-02-16 00:09:01 +0300

    0203-interactions

--- frontend/src/UserSignupPage.js
+++ frontend/src/UserSignupPage.js
@@ -1,25 +1,39 @@
 import React from 'react';
 
 class UserSignupPage extends React.Component {
+  state = {
+    username: null,
+    displayName: null,
+    password: null,
+    passwordRepeat: null
+  };
+
+  onChange = event => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
   render() {
     return (
       <form>
         <h1>Sign Up</h1>
         <div>
           <label>Username</label>
-          <input />
+          <input name="username" onChange={this.onChange} />
         </div>
         <div>
           <label>Display Name</label>
-          <input />
+          <input name="displayName" onChange={this.onChange} />
         </div>
         <div>
           <label>Password</label>
-          <input type="password" />
+          <input name="password" type="password" onChange={this.onChange} />
         </div>
         <div>
           <label>Password Repeat</label>
-          <input type="password" />
+          <input name="passwordRepeat" type="password" onChange={this.onChange} />
         </div>
         <button>Sign Up</button>
       </form>
