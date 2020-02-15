a9cb6c0517a250bb2ff35b24a81ca43649435adf
Basar Buyukkahraman <basarbk@yahoo.com>
2020-02-16 00:30:08 +0300

    0204-user-controller

---
+++ ws/src/main/java/com/hoaxify/ws/user/User.java
@@ -0,0 +1,14 @@
+package com.hoaxify.ws.user;
+
+import lombok.Data;
+
+@Data
+public class User {
+	
+	private String username;
+	
+	private String displayName;
+	
+	private String password;
+	
+}
---
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -0,0 +1,19 @@
+package com.hoaxify.ws.user;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+	
+	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+	
+	@PostMapping("/api/1.0/users")
+	public void createUser(@RequestBody User user) {
+		log.info(user.toString());
+	}
+
+}
