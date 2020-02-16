ecdd5521f89dcf7646d3b1e4809e1cefb593b385
Basar Buyukkahraman <basarbk@yahoo.com>
2020-02-16 11:51:19 +0300

    0207-saving-user-to-database

--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,7 +1,6 @@
 package com.hoaxify.ws.user;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,11 +8,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class UserController {
 	
-	private static final Logger log = LoggerFactory.getLogger(UserController.class);
+	@Autowired
+	UserRepository userRepository;
 	
 	@PostMapping("/api/1.0/users")
 	public void createUser(@RequestBody User user) {
-		log.info(user.toString());
+		userRepository.save(user);
 	}
 
 }
