217fccf2a95328a576403eac91b2ddb628125370
Basar Buyukkahraman <basarbk@yahoo.com>
2020-02-16 18:34:33 +0300

    0305-spring-validation

--- ws/src/main/java/com/hoaxify/ws/user/User.java
+++ ws/src/main/java/com/hoaxify/ws/user/User.java
@@ -3,6 +3,7 @@ package com.hoaxify.ws.user;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.NotNull;
 
 import lombok.Data;
 
@@ -14,8 +15,10 @@ public class User {
 	@GeneratedValue
 	private long id;
 	
+	@NotNull
 	private String username;
 	
+	@NotNull
 	private String displayName;
 	
 	private String password;
--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -3,6 +3,8 @@ package com.hoaxify.ws.user;
 import java.util.HashMap;
 import java.util.Map;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,7 +22,7 @@ public class UserController {
 	UserService userService;
 	
 	@PostMapping("/api/1.0/users")
-	public ResponseEntity<?> createUser(@RequestBody User user) {
+	public ResponseEntity<?> createUser(@Valid @RequestBody User user) {
 		ApiError error = new ApiError(400, "Validation error", "/api/1.0/users");
 		Map<String, String> validationErrors = new HashMap<>();
 		
