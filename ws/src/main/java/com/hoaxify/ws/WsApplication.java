135667c456bc4df4f1ccd0fe012bb65cabc2bcc3
Basar Buyukkahraman <basarbk@yahoo.com>
2020-02-16 12:15:04 +0300

    0209-hashing-password

--- ws/src/main/java/com/hoaxify/ws/WsApplication.java
+++ ws/src/main/java/com/hoaxify/ws/WsApplication.java
@@ -2,8 +2,9 @@ package com.hoaxify.ws;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class WsApplication {
 
 	public static void main(String[] args) {
--- ws/src/main/java/com/hoaxify/ws/user/UserService.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserService.java
@@ -1,5 +1,7 @@
 package com.hoaxify.ws.user;
 
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,11 +9,15 @@ public class UserService {
 	
 	UserRepository userRepository;
 	
+	PasswordEncoder passwordEncoder;
+	
 	public UserService(UserRepository userRepository) {
 		this.userRepository = userRepository;
+		this.passwordEncoder = new BCryptPasswordEncoder();
 	}
 
 	public void save(User user) {
+		user.setPassword(this.passwordEncoder.encode(user.getPassword()));
 		userRepository.save(user);
 	}
 
