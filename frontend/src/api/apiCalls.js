40ae4ce7c10f80a7cbc66d7662bf60608f064535
Basar Buyukkahraman <basarbk@yahoo.com>
2020-02-18 23:14:03 +0300

    0503-authentication

--- frontend/src/api/apiCalls.js
+++ frontend/src/api/apiCalls.js
@@ -4,6 +4,10 @@ export const signup = body => {
   return axios.post('/api/1.0/users', body);
 };
 
+export const login = creds => {
+  return axios.post('/api/1.0/auth', {}, { auth: creds });
+};
+
 export const changeLanguage = language => {
   axios.defaults.headers['accept-language'] = language;
 };
--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Input from '../components/Input';
 import { withTranslation } from 'react-i18next';
+import { login } from '../api/apiCalls';
 
 class LoginPage extends Component {
   state = {
@@ -15,6 +16,16 @@ class LoginPage extends Component {
     });
   };
 
+  onClickLogin = event => {
+    event.preventDefault();
+    const { username, password } = this.state;
+    const creds = {
+      username,
+      password
+    };
+    login(creds);
+  };
+
   render() {
     const { t } = this.props;
     return (
@@ -24,7 +35,9 @@ class LoginPage extends Component {
           <Input label={t('Username')} name="username" onChange={this.onChange} />
           <Input label={t('Password')} name="password" type="password" onChange={this.onChange} />
           <div className="text-center">
-            <button className="btn btn-primary">{t('Login')}</button>
+            <button className="btn btn-primary" onClick={this.onClickLogin}>
+              {t('Login')}
+            </button>
           </div>
         </form>
       </div>
