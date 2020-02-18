a5a7177b8ee445501f534037807f667aacaed2fe
Basar Buyukkahraman <basarbk@yahoo.com>
2020-02-18 23:14:14 +0300

    0504-command-line-runner

--- ws/src/main/java/com/hoaxify/ws/WsApplication.java
+++ ws/src/main/java/com/hoaxify/ws/WsApplication.java
@@ -1,14 +1,30 @@
 package com.hoaxify.ws;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import com.hoaxify.ws.user.User;
+import com.hoaxify.ws.user.UserService;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class WsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WsApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner createInitialUsers(UserService userService) {
+		return (args) -> {
+			User user = new User();
+			user.setUsername("user1");
+			user.setDisplayName("display1");
+			user.setPassword("P4ssword");
+			userService.save(user);
+		};
 	}
 
 }
