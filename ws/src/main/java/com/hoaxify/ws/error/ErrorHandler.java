e5cb473eb363ae8d66917a428c97f802f13602b4
Basar Buyukkahraman <basarbk@yahoo.com>
2020-02-19 23:56:20 +0300

    0509-error-handler-for-all

--- ws/src/main/java/com/hoaxify/ws/error/ErrorHandler.java
+++ ws/src/main/java/com/hoaxify/ws/error/ErrorHandler.java
@@ -1,10 +1,13 @@
 package com.hoaxify.ws.error;
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.servlet.error.ErrorAttributes;
 import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.context.request.WebRequest;
@@ -21,7 +24,17 @@ public class ErrorHandler implements ErrorController {
 		String message = (String)attributes.get("message");
 		String path = (String) attributes.get("path");
 		int status = (Integer) attributes.get("status");
-		return new ApiError(status, message, path);
+		ApiError error = new ApiError(status, message, path);
+		if(attributes.containsKey("errors")) {
+			@SuppressWarnings("unchecked")
+			List<FieldError> fieldErrors = (List<FieldError>)attributes.get("errors");
+			Map<String, String> validationErrors = new HashMap<>();
+			for(FieldError fieldError: fieldErrors) {
+				validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
+			}
+			error.setValidationErrors(validationErrors);
+		}
+		return error; 
 	}
 	
 
--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,21 +1,12 @@
 package com.hoaxify.ws.user;
 
-import java.util.HashMap;
-import java.util.Map;
-
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hoaxify.ws.error.ApiError;
 import com.hoaxify.ws.shared.GenericResponse;
 
 @RestController
@@ -30,16 +21,4 @@ public class UserController {
 		return new GenericResponse("user created");
 	}
 	
-	@ExceptionHandler(MethodArgumentNotValidException.class)
-	@ResponseStatus(HttpStatus.BAD_REQUEST)
-	public ApiError handleValidationException(MethodArgumentNotValidException exception) {
-		ApiError error = new ApiError(400, "Validation error", "/api/1.0/users");
-		Map<String, String> validationErrors = new HashMap<>();
-		for(FieldError fieldError: exception.getBindingResult().getFieldErrors()) {
-			validationErrors.put(fieldError.getField(), fieldError.getDefaultMessage());
-		}
-		error.setValidationErrors(validationErrors);
-		return error;
-	}
-
 }
