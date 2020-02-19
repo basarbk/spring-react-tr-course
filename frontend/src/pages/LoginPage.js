2dc28f20c3a2a804cb287a0f9a935f31d05fadef
Basar Buyukkahraman <basarbk@yahoo.com>
2020-02-20 00:30:10 +0300

    0511-login-error-on-client

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -6,36 +6,52 @@ import { login } from '../api/apiCalls';
 class LoginPage extends Component {
   state = {
     username: null,
-    password: null
+    password: null,
+    error: null
   };
 
   onChange = event => {
     const { name, value } = event.target;
     this.setState({
-      [name]: value
+      [name]: value,
+      error: null
     });
   };
 
-  onClickLogin = event => {
+  onClickLogin = async event => {
     event.preventDefault();
     const { username, password } = this.state;
     const creds = {
       username,
       password
     };
-    login(creds);
+    this.setState({
+      error: null
+    });
+    try {
+      await login(creds);
+    } catch (apiError) {
+      this.setState({
+        error: apiError.response.data.message
+      });
+    }
   };
 
   render() {
     const { t } = this.props;
+    const { username, password, error } = this.state;
+
+    const buttonEnabled = username && password;
+
     return (
       <div className="container">
         <form>
           <h1 className="text-center">{t('Login')}</h1>
           <Input label={t('Username')} name="username" onChange={this.onChange} />
           <Input label={t('Password')} name="password" type="password" onChange={this.onChange} />
+          {error && <div className="alert alert-danger">{error}</div>}
           <div className="text-center">
-            <button className="btn btn-primary" onClick={this.onClickLogin}>
+            <button className="btn btn-primary" onClick={this.onClickLogin} disabled={!buttonEnabled}>
               {t('Login')}
             </button>
           </div>
