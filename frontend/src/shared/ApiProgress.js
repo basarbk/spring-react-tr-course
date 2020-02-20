34038c82c5b6af55dba057e22d6a97916f0d5070
Basar Buyukkahraman <basarbk@yahoo.com>
2020-02-21 00:43:38 +0300

    0704-component-will-unmount

--- frontend/src/shared/ApiProgress.js
+++ frontend/src/shared/ApiProgress.js
@@ -14,12 +14,12 @@ export function withApiProgress(WrappedComponent, apiPath) {
     };
 
     componentDidMount() {
-      axios.interceptors.request.use(request => {
+      this.requestInterceptor = axios.interceptors.request.use(request => {
         this.updateApiCallFor(request.url, true);
         return request;
       });
 
-      axios.interceptors.response.use(
+      this.responseInterceptor = axios.interceptors.response.use(
         response => {
           this.updateApiCallFor(response.config.url, false);
           return response;
@@ -29,6 +29,11 @@ export function withApiProgress(WrappedComponent, apiPath) {
           throw error;
         }
       );
+    }
+
+    componentWillUnmount() {
+      axios.interceptors.request.eject(this.requestInterceptor);
+      axios.interceptors.response.eject(this.responseInterceptor);
     }
 
     updateApiCallFor = (url, inProgress) => {
