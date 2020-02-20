b04145b8cc4d172abb6d41c6e06d640c3c552ef1
Basar Buyukkahraman <basarbk@yahoo.com>
2020-02-20 23:45:43 +0300

    0701-new-pages

--- frontend/src/container/App.js
+++ frontend/src/container/App.js
@@ -2,16 +2,13 @@ import React from 'react';
 import UserSignupPage from '../pages/UserSignupPage';
 import LoginPage from '../pages/LoginPage';
 import LanguageSelector from '../components/LanguageSelector';
+import HomePage from '../pages/HomePage';
+import UserPage from '../pages/UserPage';
 
 function App() {
   return (
     <div className="row">
-      <div className="col">
-        <UserSignupPage />
-      </div>
-      <div className="col">
-        <LoginPage />
-      </div>
+      <UserPage />
       <LanguageSelector />
     </div>
   );
---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const HomePage = () => {
+  return <div className="container">homepage</div>;
+};
+
+export default HomePage;
