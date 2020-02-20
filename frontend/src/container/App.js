554d4087e1dd191ba00368640fae0bd8a69a5345
Basar Buyukkahraman <basarbk@yahoo.com>
2020-02-21 00:03:49 +0300

    0702-react-router

--- frontend/src/container/App.js
+++ frontend/src/container/App.js
@@ -4,11 +4,20 @@ import LoginPage from '../pages/LoginPage';
 import LanguageSelector from '../components/LanguageSelector';
 import HomePage from '../pages/HomePage';
 import UserPage from '../pages/UserPage';
+import { HashRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="row">
-      <UserPage />
+    <div>
+      <HashRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/login" component={LoginPage} />
+          <Route path="/signup" component={UserSignupPage} />
+          <Route path="/user/:username" component={UserPage} />
+          <Redirect to="/" />
+        </Switch>
+      </HashRouter>
       <LanguageSelector />
     </div>
   );
