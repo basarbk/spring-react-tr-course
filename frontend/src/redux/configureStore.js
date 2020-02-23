6b9bd10aa514f9590397e7a5780d4461ecd4e878
Basar Buyukkahraman <basarbk@yahoo.com>
2020-02-23 12:29:21 +0300

    0809-persisting-state

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,16 +1,30 @@
 import { createStore } from 'redux';
 import authReducer from './authReducer';
 
-const loggedInState = {
-  isLoggedIn: true,
-  username: 'user1',
-  displayName: 'display1',
-  image: null,
-  password: 'P4ssword'
-};
-
 const configureStore = () => {
-  return createStore(authReducer, loggedInState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+  const hoaxAuth = localStorage.getItem('hoax-auth');
+
+  let stateInLocalStorage = {
+    isLoggedIn: false,
+    username: undefined,
+    displayName: undefined,
+    image: undefined,
+    password: undefined
+  };
+
+  if (hoaxAuth) {
+    try {
+      stateInLocalStorage = JSON.parse(hoaxAuth);
+    } catch (error) {}
+  }
+
+  const store = createStore(authReducer, stateInLocalStorage, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+
+  store.subscribe(() => {
+    localStorage.setItem('hoax-auth', JSON.stringify(store.getState()));
+  });
+
+  return store;
 };
 
 export default configureStore;
