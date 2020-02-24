e415a523fb59168f8f78203d2c1cde8995697575
Basar Buyukkahraman <basarbk@yahoo.com>
2020-02-25 00:19:18 +0300

    0905-useApiProgress

--- frontend/src/pages/LoginPage.js
+++ frontend/src/pages/LoginPage.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import Input from '../components/Input';
 import { useTranslation } from 'react-i18next';
 import ButtonWithProgress from '../components/ButtonWithProgress';
-import { withApiProgress } from '../shared/ApiProgress';
+import { useApiProgress } from '../shared/ApiProgress';
 import { useDispatch } from 'react-redux';
 import { loginHandler } from '../redux/authActions';
 
@@ -38,7 +38,7 @@ const LoginPage = props => {
 
   const { t } = useTranslation();
 
-  const { pendingApiCall } = props;
+  const pendingApiCall = useApiProgress('/api/1.0/auth');
 
   const buttonEnabled = username && password;
 
@@ -57,4 +57,4 @@ const LoginPage = props => {
   );
 };
 
-export default withApiProgress(LoginPage, '/api/1.0/auth');
+export default LoginPage;
