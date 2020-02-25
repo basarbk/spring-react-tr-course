3edaf9adb8262fb6f53610664a71ffd5f12edc3e
Basar Buyukkahraman <basarbk@yahoo.com>
2020-02-25 23:51:11 +0300

    1004-pagination

--- ws/src/main/java/com/hoaxify/ws/WsApplication.java
+++ ws/src/main/java/com/hoaxify/ws/WsApplication.java
@@ -18,7 +18,7 @@ public class WsApplication {
 	@Bean
 	CommandLineRunner createInitialUsers(UserService userService) {
 		return (args) -> {
-			for(int i = 1; i<=10;i++) {				
+			for(int i = 1; i<=25;i++) {				
 				User user = new User();
 				user.setUsername("user"+i);
 				user.setDisplayName("display"+i);
