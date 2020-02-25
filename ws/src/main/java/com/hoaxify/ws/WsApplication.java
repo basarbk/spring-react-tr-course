cc7746ad0f163c57db6f6e34e126be032c1ed483
Basar Buyukkahraman <basarbk@yahoo.com>
2020-02-25 22:52:39 +0300

    1001-generating-users

--- ws/src/main/java/com/hoaxify/ws/WsApplication.java
+++ ws/src/main/java/com/hoaxify/ws/WsApplication.java
@@ -18,11 +18,13 @@ public class WsApplication {
 	@Bean
 	CommandLineRunner createInitialUsers(UserService userService) {
 		return (args) -> {
-			User user = new User();
-			user.setUsername("user1");
-			user.setDisplayName("display1");
-			user.setPassword("P4ssword");
-			userService.save(user);
+			for(int i = 1; i<=10;i++) {				
+				User user = new User();
+				user.setUsername("user"+i);
+				user.setDisplayName("display"+i);
+				user.setPassword("P4ssword");
+				userService.save(user);
+			}
 		};
 	}
 
