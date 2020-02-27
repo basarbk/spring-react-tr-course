388d7534963c764f191f232980c610ee368bffff
Basar Buyukkahraman <basarbk@yahoo.com>
2020-02-27 23:18:42 +0300

    1102-load-user-data-in-user-page

--- frontend/src/pages/UserPage.js
+++ frontend/src/pages/UserPage.js
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ProfileCard from '../components/ProfileCard';
+import { getUser } from '../api/apiCalls';
+import { useParams } from 'react-router-dom';
 
 const UserPage = props => {
+  const [user, setUser] = useState();
+
+  const { username } = useParams();
+
+  useEffect(() => {
+    const loadUser = async () => {
+      try {
+        const response = await getUser(username);
+        setUser(response.data);
+      } catch (error) {}
+    };
+    loadUser();
+  }, [username]);
+
   return (
     <div className="container">
       <ProfileCard />
