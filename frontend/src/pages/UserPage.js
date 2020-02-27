9b21c8254cfa24030829ec8ca72a94211eec9122
Basar Buyukkahraman <basarbk@yahoo.com>
2020-02-27 23:34:32 +0300

    1103-error-feedback

--- frontend/src/pages/UserPage.js
+++ frontend/src/pages/UserPage.js
@@ -2,21 +2,46 @@ import React, { useState, useEffect } from 'react';
 import ProfileCard from '../components/ProfileCard';
 import { getUser } from '../api/apiCalls';
 import { useParams } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
-const UserPage = props => {
+const UserPage = () => {
   const [user, setUser] = useState();
+  const [notFound, setNotFound] = useState(false);
 
   const { username } = useParams();
+
+  const { t } = useTranslation();
+
+  useEffect(() => {
+    setNotFound(false);
+  }, [user]);
 
   useEffect(() => {
     const loadUser = async () => {
       try {
         const response = await getUser(username);
         setUser(response.data);
-      } catch (error) {}
+      } catch (error) {
+        setNotFound(true);
+      }
     };
     loadUser();
   }, [username]);
+
+  if (notFound) {
+    return (
+      <div className="container">
+        <div className="alert alert-danger text-center">
+          <div>
+            <i className="material-icons" style={{ fontSize: '48px' }}>
+              error
+            </i>
+          </div>
+          {t('User not found')}
+        </div>
+      </div>
+    );
+  }
 
   return (
     <div className="container">
