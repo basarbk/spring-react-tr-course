be779bf63bc62082e6dda25a626c25cf1c21211a
Basar Buyukkahraman <basarbk@yahoo.com>
2020-02-28 01:03:34 +0300

    1203-update-user-controller

--- ws/src/main/java/com/hoaxify/ws/user/UserController.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserController.java
@@ -8,12 +8,14 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hoaxify.ws.shared.CurrentUser;
 import com.hoaxify.ws.shared.GenericResponse;
+import com.hoaxify.ws.user.vm.UserUpdateVM;
 import com.hoaxify.ws.user.vm.UserVM;
 
 @RestController
@@ -38,5 +40,12 @@ public class UserController {
 	UserVM getUser(@PathVariable String username) {
 		User user = userService.getByUsername(username);
 		return new UserVM(user);
+	}
+	
+	@PutMapping("/users/{username}")
+	UserVM updateUser(@RequestBody UserUpdateVM updatedUser, @PathVariable String username) {
+		User user = userService.updateUser(username, updatedUser);
+		return new UserVM(user);
+		
 	}
 }
---
+++ ws/src/main/java/com/hoaxify/ws/user/vm/UserUpdateVM.java
@@ -0,0 +1,10 @@
+package com.hoaxify.ws.user.vm;
+
+import lombok.Data;
+
+@Data
+public class UserUpdateVM {
+	
+	private String displayName;
+
+}
