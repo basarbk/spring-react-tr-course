62ce56eab17acebdc63d10c58150e411249378d0
Basar Buyukkahraman <basarbk@yahoo.com>
2020-02-28 00:33:17 +0300

    1201-update-layout

--- frontend/src/components/ProfileCard.js
+++ frontend/src/components/ProfileCard.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileImageWithDefault from './ProfileImageWithDefault';
+import { useTranslation } from 'react-i18next';
+import Input from './Input';
 
 const ProfileCard = props => {
+  const [inEditMode, setInEditMode] = useState(false);
   const { username: loggedInUsername } = useSelector(store => ({ username: store.username }));
   const routeParams = useParams();
+
+  const { t } = useTranslation();
 
   const { user } = props;
   const { username, displayName, image } = user;
@@ -22,9 +27,32 @@ const ProfileCard = props => {
         <ProfileImageWithDefault className="rounded-circle shadow" width="200" height="200" alt={`${username} profile`} image={image} />
       </div>
       <div className="card-body">
-        <h3>
-          {displayName}@{username}
-        </h3>
+        {!inEditMode && (
+          <>
+            <h3>
+              {displayName}@{username}
+            </h3>
+            <button className="btn btn-success d-inline-flex" onClick={() => setInEditMode(true)}>
+              <i className="material-icons">edit</i>
+              {t('Edit')}
+            </button>
+          </>
+        )}
+        {inEditMode && (
+          <div>
+            <Input label={t('Change Display Name')} />
+            <div>
+              <button className="btn btn-primary d-inline-flex">
+                <i className="material-icons">save</i>
+                {t('Save')}
+              </button>
+              <button className="btn btn-light d-inline-flex ml-1" onClick={() => setInEditMode(false)}>
+                <i className="material-icons">close</i>
+                {t('Cancel')}
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -17,7 +17,11 @@ i18n.use(initReactI18next).init({
         Next: 'next >',
         Previous: '< previous',
         'Load Failure': 'Load Failure',
-        'User not found': 'User not found'
+        'User not found': 'User not found',
+        Edit: 'Edit',
+        'Change Display Name': 'Change Display Name',
+        Save: 'Save',
+        Cancel: 'Cancel'
       }
     },
     tr: {
@@ -34,7 +38,11 @@ i18n.use(initReactI18next).init({
         Next: 'sonraki >',
         Previous: '< önceki',
         'Load Failure': 'Liste alınamadı',
-        'User not found': 'Kullanıcı bulunamadı'
+        'User not found': 'Kullanıcı bulunamadı',
+        Edit: 'Düzenle',
+        'Change Display Name': 'Görünür İsminizi Değiştirin',
+        Save: 'Kaydet',
+        Cancel: 'İptal Et'
       }
     }
   },
