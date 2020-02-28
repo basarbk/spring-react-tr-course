d044357d82c780813fa262e7c721a7a2a6a8cb64
Basar Buyukkahraman <basarbk@yahoo.com>
2020-02-29 00:12:40 +0300

    1301-file-upload-layout

--- frontend/src/components/ProfileImageWithDefault.js
+++ frontend/src/components/ProfileImageWithDefault.js
@@ -2,13 +2,13 @@ import React from 'react';
 import defaultPicture from '../assets/profile.png';
 
 const ProfileImageWithDefault = props => {
-  const { image } = props;
+  const { image, tempImage } = props;
 
   let imageSource = defaultPicture;
   if (image) {
     imageSource = image;
   }
-  return <img alt={`Profile`} src={imageSource} {...props} />;
+  return <img alt={`Profile`} src={tempImage || imageSource} {...props} />;
 };
 
 export default ProfileImageWithDefault;
