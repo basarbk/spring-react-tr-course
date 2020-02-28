03c0cfbbe859e5fc73d2b7aebeefccf236e8e62b
Basar Buyukkahraman <basarbk@yahoo.com>
2020-02-29 00:12:40 +0300

    1301-file-upload-layout

--- frontend/src/components/ProfileCard.js
+++ frontend/src/components/ProfileCard.js
@@ -16,6 +16,7 @@ const ProfileCard = props => {
   const pathUsername = routeParams.username;
   const [user, setUser] = useState({});
   const [editable, setEditable] = useState(false);
+  const [newImage, setNewImage] = useState();
 
   useEffect(() => {
     setUser(props.user);
@@ -31,6 +32,7 @@ const ProfileCard = props => {
   useEffect(() => {
     if (!inEditMode) {
       setUpdatedDisplayName(undefined);
+      setNewImage(undefined);
     } else {
       setUpdatedDisplayName(displayName);
     }
@@ -47,12 +49,28 @@ const ProfileCard = props => {
     } catch (error) {}
   };
 
+  const onChangeFile = event => {
+    const file = event.target.files[0];
+    const fileReader = new FileReader();
+    fileReader.onloadend = () => {
+      setNewImage(fileReader.result);
+    };
+    fileReader.readAsDataURL(file);
+  };
+
   const pendingApiCall = useApiProgress('put', '/api/1.0/users/' + username);
 
   return (
     <div className="card text-center">
       <div className="card-header">
-        <ProfileImageWithDefault className="rounded-circle shadow" width="200" height="200" alt={`${username} profile`} image={image} />
+        <ProfileImageWithDefault
+          className="rounded-circle shadow"
+          width="200"
+          height="200"
+          alt={`${username} profile`}
+          image={image}
+          tempImage={newImage}
+        />
       </div>
       <div className="card-body">
         {!inEditMode && (
@@ -77,6 +95,7 @@ const ProfileCard = props => {
                 setUpdatedDisplayName(event.target.value);
               }}
             />
+            <input type="file" onChange={onChangeFile} />
             <div>
               <ButtonWithProgress
                 className="btn btn-primary d-inline-flex"
