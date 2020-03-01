0f78261ccc31a04ec65db1425227be9bf58b1525
Basar Buyukkahraman <basarbk@yahoo.com>
2020-03-01 22:34:05 +0300

    1406-profile-info-on-topbar

--- frontend/src/components/TopBar.js
+++ frontend/src/components/TopBar.js
@@ -4,13 +4,16 @@ import { Link } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { useDispatch, useSelector } from 'react-redux';
 import { logoutSuccess } from '../redux/authActions';
+import ProfileImageWithDefault from './ProfileImageWithDefault';
 
 const TopBar = props => {
   const { t } = useTranslation();
 
-  const { username, isLoggedIn } = useSelector(store => ({
+  const { username, isLoggedIn, displayName, image } = useSelector(store => ({
     isLoggedIn: store.isLoggedIn,
-    username: store.username
+    username: store.username,
+    displayName: store.displayName,
+    image: store.image
   }));
 
   const dispatch = useDispatch();
@@ -36,13 +39,21 @@ const TopBar = props => {
   if (isLoggedIn) {
     links = (
       <ul className="navbar-nav ml-auto">
-        <li>
-          <Link className="nav-link" to={`/user/${username}`}>
-            {username}
-          </Link>
-        </li>
-        <li className="nav-link" onClick={onLogoutSuccess} style={{ cursor: 'pointer' }}>
-          {t('Logout')}
+        <li className="nav-item dropdown">
+          <div className="d-flex" style={{ cursor: 'pointer' }}>
+            <ProfileImageWithDefault image={image} width="32" height="32" className="rounded-circle m-auto" />
+            <span className="nav-link dropdown-toggle">{displayName}</span>
+          </div>
+          <div className="dropdown-menu show p-0 shadow">
+            <Link className="dropdown-item d-flex p-2" to={`/user/${username}`}>
+              <i className="material-icons text-info mr-2">person</i>
+              {t('My Profile')}
+            </Link>
+            <span className="dropdown-item  d-flex p-2" onClick={onLogoutSuccess} style={{ cursor: 'pointer' }}>
+              <i className="material-icons text-danger mr-2">power_settings_new</i>
+              {t('Logout')}
+            </span>
+          </div>
         </li>
       </ul>
     );
