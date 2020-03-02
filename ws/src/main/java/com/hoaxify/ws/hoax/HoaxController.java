a0738da22a0b69bdaa157b877a1d91ee4bde2ebd
Basar Buyukkahraman <basarbk@yahoo.com>
2020-03-02 14:56:34 +0300

    1601-hoax-get-controller

--- ws/src/main/java/com/hoaxify/ws/hoax/HoaxController.java
+++ ws/src/main/java/com/hoaxify/ws/hoax/HoaxController.java
@@ -3,22 +3,32 @@ package com.hoaxify.ws.hoax;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hoaxify.ws.shared.GenericResponse;
 
 @RestController
+@RequestMapping("/api/1.0")
 public class HoaxController {
 	
 	@Autowired
 	HoaxService hoaxService;
 	
-	@PostMapping("/api/1.0/hoaxes")
+	@PostMapping("/hoaxes")
 	GenericResponse saveHoax(@Valid @RequestBody Hoax hoax) {
 		hoaxService.save(hoax);
 		return new GenericResponse("Hoax is saved");
+	}
+	
+	@GetMapping("/hoaxes")
+	Page<Hoax> getHoaxes(Pageable page){
+		return hoaxService.getHoaxes(page);
 	}
 
 }
