e4040d67848edd3765c6f50a0bcc6e562986e662
Basar Buyukkahraman <basarbk@yahoo.com>
2020-03-02 21:28:07 +0300

    1704-hoax-view-with-user

--- frontend/src/components/HoaxView.js
+++ frontend/src/components/HoaxView.js
@@ -1,8 +1,26 @@
 import React from 'react';
+import ProfileImageWithDefault from './ProfileImageWithDefault';
+import { Link } from 'react-router-dom';
 
 const HoaxView = props => {
   const { hoax } = props;
-  return <div className="card p-1">{hoax.content}</div>;
+  const { user, content } = hoax;
+  const { username, displayName, image } = user;
+  return (
+    <div className="card p-1">
+      <div className="d-flex">
+        <ProfileImageWithDefault image={image} width="32" height="32" className="rounded-circle m-1" />
+        <div className="flex-fill m-auto pl-2">
+          <Link to={`/user/${username}`} className="text-dark">
+            <h6>
+              {displayName}@{username}
+            </h6>
+          </Link>
+        </div>
+      </div>
+      <div className="pl-5">{content}</div>
+    </div>
+  );
 };
 
 export default HoaxView;
