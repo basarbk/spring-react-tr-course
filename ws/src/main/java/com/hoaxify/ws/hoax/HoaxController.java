928c78689161b3258d8899645bc15179e9965f9a
Basar Buyukkahraman <basarbk@yahoo.com>
2020-03-02 23:43:41 +0300

    1707-get-hoaxes-of-user

--- ws/src/main/java/com/hoaxify/ws/hoax/HoaxController.java
+++ ws/src/main/java/com/hoaxify/ws/hoax/HoaxController.java
@@ -8,6 +8,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -35,5 +36,9 @@ public class HoaxController {
 	Page<HoaxVM> getHoaxes(@PageableDefault(sort = "id", direction = Direction.DESC) Pageable page){
 		return hoaxService.getHoaxes(page).map(HoaxVM::new);
 	}
-
+	
+	@GetMapping("/users/{username}/hoaxes") 
+	Page<HoaxVM> getUserHoaxes(@PathVariable String username, @PageableDefault(sort = "id", direction = Direction.DESC) Pageable page){
+		return hoaxService.getHoaxesOfUser(username, page).map(HoaxVM::new);
+	}
 }
