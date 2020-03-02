565f3145f8da359f5ab3788f45817c2007ea3074
Basar Buyukkahraman <basarbk@yahoo.com>
2020-03-02 23:18:12 +0300

    1705-relative-time

--- frontend/src/i18n.js
+++ frontend/src/i18n.js
@@ -1,5 +1,6 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import { register } from 'timeago.js';
 
 i18n.use(initReactI18next).init({
   resources: {
@@ -64,5 +65,25 @@ i18n.use(initReactI18next).init({
     wait: true
   }
 });
+
+const timeageTR = (number, index) => {
+  return [
+    ['az önce', 'şimdi'],
+    ['%s saniye önce', '%s saniye içinde'],
+    ['1 dakika önce', '1 dakika içinde'],
+    ['%s dakika önce', '%s dakika içinde'],
+    ['1 saat önce', '1 saat içinde'],
+    ['%s saat önce', '%s saat içinde'],
+    ['1 gün önce', '1 gün içinde'],
+    ['%s gün önce', '%s gün içinde'],
+    ['1 hafta önce', '1 hafta içinde'],
+    ['%s hafta önce', '%s hafta içinde'],
+    ['1 ay önce', '1 ay içinde'],
+    ['%s ay önce', '%s ay içinde'],
+    ['1 yıl önce', '1 yıl içinde'],
+    ['%s yıl önce', '%s yıl içinde']
+  ][index];
+};
+register('tr', timeageTR);
 
 export default i18n;
