a6c37dd94d865d9f0aeb2ec93fc1c09d367709b9
Basar Buyukkahraman <basarbk@yahoo.com>
2020-03-02 15:12:29 +0300

    1603-hoaxes-in-clr

--- ws/src/main/java/com/hoaxify/ws/WsApplication.java
+++ ws/src/main/java/com/hoaxify/ws/WsApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Profile;
 
+import com.hoaxify.ws.hoax.Hoax;
+import com.hoaxify.ws.hoax.HoaxService;
 import com.hoaxify.ws.user.User;
 import com.hoaxify.ws.user.UserService;
 
@@ -18,7 +20,7 @@ public class WsApplication {
 	
 	@Bean
 	@Profile("dev")
-	CommandLineRunner createInitialUsers(UserService userService) {
+	CommandLineRunner createInitialUsers(UserService userService, HoaxService hoaxService) {
 		return (args) -> {
 			for(int i = 1; i<=25;i++) {				
 				User user = new User();
@@ -26,6 +28,11 @@ public class WsApplication {
 				user.setDisplayName("display"+i);
 				user.setPassword("P4ssword");
 				userService.save(user);
+			}
+			for(int i = 1;i<=50;i++) {
+				Hoax hoax = new Hoax();
+				hoax.setContent("hoax - " +i);
+				hoaxService.save(hoax);
 			}
 		};
 	}
