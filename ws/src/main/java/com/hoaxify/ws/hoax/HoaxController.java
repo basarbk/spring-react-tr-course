35b691af7e85781f41c92ad4f3c8bb0e9282efad
Basar Buyukkahraman <basarbk@yahoo.com>
2020-03-02 15:24:45 +0300

    1605-sorting

--- ws/src/main/java/com/hoaxify/ws/hoax/HoaxController.java
+++ ws/src/main/java/com/hoaxify/ws/hoax/HoaxController.java
@@ -5,6 +5,8 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,7 +29,7 @@ public class HoaxController {
 	}
 	
 	@GetMapping("/hoaxes")
-	Page<Hoax> getHoaxes(Pageable page){
+	Page<Hoax> getHoaxes(@PageableDefault(sort = "id", direction = Direction.DESC) Pageable page){
 		return hoaxService.getHoaxes(page);
 	}
 
