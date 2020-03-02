e9c1c2abaea8e7ad3871ec28fae5f02e8c3ca228
Basar Buyukkahraman <basarbk@yahoo.com>
2020-03-02 16:42:50 +0300

    1607-loading-old-hoaxes

--- frontend/src/api/apiCalls.js
+++ frontend/src/api/apiCalls.js
@@ -37,6 +37,6 @@ export const postHoax = hoax => {
   return axios.post('/api/1.0/hoaxes', hoax);
 };
 
-export const getHoaxes = () => {
-  return axios.get('/api/1.0/hoaxes');
+export const getHoaxes = (page = 0) => {
+  return axios.get('/api/1.0/hoaxes?page=' + page);
 };
--- frontend/src/components/HoaxFeed.js
+++ frontend/src/components/HoaxFeed.js
@@ -4,19 +4,24 @@ import { useTranslation } from 'react-i18next';
 import HoaxView from './HoaxView';
 
 const HoaxFeed = () => {
-  const [hoaxPage, setHoaxPage] = useState({ content: [], last: true });
+  const [hoaxPage, setHoaxPage] = useState({ content: [], last: true, number: 0 });
   const { t } = useTranslation();
 
   useEffect(() => {
-    const loadHoaxes = async () => {
-      try {
-        const response = await getHoaxes();
-        setHoaxPage(response.data);
-      } catch (error) {}
-    };
     loadHoaxes();
   }, []);
-  const { content, last } = hoaxPage;
+
+  const loadHoaxes = async page => {
+    try {
+      const response = await getHoaxes(page);
+      setHoaxPage(previousHoaxPage => ({
+        ...response.data,
+        content: [...previousHoaxPage.content, ...response.data.content]
+      }));
+    } catch (error) {}
+  };
+
+  const { content, last, number } = hoaxPage;
 
   if (content.length === 0) {
     return <div className="alert alert-secondary text-center">{t('There are no hoaxes')}</div>;
@@ -27,7 +32,11 @@ const HoaxFeed = () => {
       {content.map(hoax => {
         return <HoaxView key={hoax.id} hoax={hoax} />;
       })}
-      {!last && <div className="alert alert-secondary text-center">{t('Load old hoaxes')}</div>}
+      {!last && (
+        <div className="alert alert-secondary text-center" style={{ cursor: 'pointer' }} onClick={() => loadHoaxes(number + 1)}>
+          {t('Load old hoaxes')}
+        </div>
+      )}
     </div>
   );
 };
