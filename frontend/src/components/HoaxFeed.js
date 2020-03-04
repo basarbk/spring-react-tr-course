619d5211c8b4f3930380fdee11c9600ca142b9b4
Basar Buyukkahraman <basarbk@yahoo.com>
2020-03-04 23:07:31 +0300

    1608-progress

--- frontend/src/components/HoaxFeed.js
+++ frontend/src/components/HoaxFeed.js
@@ -2,10 +2,14 @@ import React, { useState, useEffect } from 'react';
 import { getHoaxes } from '../api/apiCalls';
 import { useTranslation } from 'react-i18next';
 import HoaxView from './HoaxView';
+import { useApiProgress } from '../shared/ApiProgress';
+import Spinner from './Spinner';
 
 const HoaxFeed = () => {
   const [hoaxPage, setHoaxPage] = useState({ content: [], last: true, number: 0 });
   const { t } = useTranslation();
+
+  const pendingApiCall = useApiProgress('get', '/api/1.0/hoaxes');
 
   useEffect(() => {
     loadHoaxes();
@@ -24,7 +28,7 @@ const HoaxFeed = () => {
   const { content, last, number } = hoaxPage;
 
   if (content.length === 0) {
-    return <div className="alert alert-secondary text-center">{t('There are no hoaxes')}</div>;
+    return <div className="alert alert-secondary text-center">{pendingApiCall ? <Spinner /> : t('There are no hoaxes')}</div>;
   }
 
   return (
@@ -33,8 +37,12 @@ const HoaxFeed = () => {
         return <HoaxView key={hoax.id} hoax={hoax} />;
       })}
       {!last && (
-        <div className="alert alert-secondary text-center" style={{ cursor: 'pointer' }} onClick={() => loadHoaxes(number + 1)}>
-          {t('Load old hoaxes')}
+        <div
+          className="alert alert-secondary text-center"
+          style={{ cursor: pendingApiCall ? 'not-allowed' : 'pointer' }}
+          onClick={pendingApiCall ? () => {} : () => loadHoaxes(number + 1)}
+        >
+          {pendingApiCall ? <Spinner /> : t('Load old hoaxes')}
         </div>
       )}
     </div>
