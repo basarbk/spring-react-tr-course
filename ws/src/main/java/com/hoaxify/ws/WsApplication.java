87a82c5310f470b14cdd2df44c60aa84732de3ec
Basar Buyukkahraman <basarbk@yahoo.com>
2020-03-05 22:50:16 +0300

    1803-old-hoaxes-for-user

--- ws/src/main/java/com/hoaxify/ws/WsApplication.java
+++ ws/src/main/java/com/hoaxify/ws/WsApplication.java
@@ -28,7 +28,7 @@ public class WsApplication {
 				user.setDisplayName("display"+i);
 				user.setPassword("P4ssword");
 				userService.save(user);
-				for(int j = 1;j<=2;j++) {
+				for(int j = 1;j<=20;j++) {
 					Hoax hoax = new Hoax();
 					hoax.setContent("hoax (" +j + ") from user ("+i+")");
 					hoaxService.save(hoax, user);
