6e0d6b1c164e67795ce5a981581cbebbef9288b1
Basar Buyukkahraman <basarbk@yahoo.com>
2020-03-06 00:02:17 +0300

    1806-getting-new-hoaxes

--- ws/src/main/java/com/hoaxify/ws/hoax/HoaxRepository.java
+++ ws/src/main/java/com/hoaxify/ws/hoax/HoaxRepository.java
@@ -1,7 +1,10 @@
 package com.hoaxify.ws.hoax;
 
+import java.util.List;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.hoaxify.ws.user.User;
@@ -17,5 +20,7 @@ public interface HoaxRepository extends JpaRepository<Hoax, Long>{
 	long countByIdGreaterThan(long id);
 	
 	long countByIdGreaterThanAndUser(long id, User user);
+	
+	List<Hoax> findByIdGreaterThan(long id, Sort sort);
 
 }
