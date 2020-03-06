0d5e91dc3ca23bfd5ce725fe4d714eeb31be32a0
Basar Buyukkahraman <basarbk@yahoo.com>
2020-03-07 00:55:25 +0300

    1905-file-attachment-entity

---
+++ ws/src/main/java/com/hoaxify/ws/file/FileAttachmentRepository.java
@@ -0,0 +1,7 @@
+package com.hoaxify.ws.file;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FileAttachmentRepository extends JpaRepository<FileAttachment, Long>{
+
+}
