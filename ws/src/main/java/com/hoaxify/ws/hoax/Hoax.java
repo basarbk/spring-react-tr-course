a7afcd870b6793db3c9c0f701211ad88dd5bd4e2
Basar Buyukkahraman <basarbk@yahoo.com>
2020-03-08 00:19:58 +0300

    2005-cleanup-attachment

--- ws/src/main/java/com/hoaxify/ws/hoax/Hoax.java
+++ ws/src/main/java/com/hoaxify/ws/hoax/Hoax.java
@@ -2,6 +2,7 @@ package com.hoaxify.ws.hoax;
 
 import java.util.Date;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -33,6 +34,6 @@ public class Hoax {
 	@ManyToOne
 	private User user;
 	
-	@OneToOne(mappedBy = "hoax")
+	@OneToOne(mappedBy = "hoax", cascade = CascadeType.REMOVE)
 	private FileAttachment fileAttachment;
 }
