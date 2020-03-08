a78ef2119666d21d11232c2a3de6c26ac31eb2d7
Basar Buyukkahraman <basarbk@yahoo.com>
2020-03-08 23:54:00 +0300

    2009-custom-delete-repo-method

--- ws/src/main/java/com/hoaxify/ws/user/UserRepository.java
+++ ws/src/main/java/com/hoaxify/ws/user/UserRepository.java
@@ -1,5 +1,7 @@
 package com.hoaxify.ws.user;
 
+import javax.transaction.Transactional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +12,6 @@ public interface UserRepository extends JpaRepository<User, Long>{
 	
 	Page<User> findByUsernameNot(String username, Pageable page);
 	
+	@Transactional
+	void deleteByUsername(String username);
 }
