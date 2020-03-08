e2bcd447e301a5b30507fd6ea03498094da1fbdf
Basar Buyukkahraman <basarbk@yahoo.com>
2020-03-08 22:58:52 +0300

    2006-modal

---
+++ frontend/src/components/Modal.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { useTranslation } from 'react-i18next';
+import ButtonWithProgress from './ButtonWithProgress';
+
+const Modal = props => {
+  const { visible, onClickCancel, message, onClickOk, pendingApiCall } = props;
+  const { t } = useTranslation();
+
+  let className = 'modal fade';
+  if (visible) {
+    className += ' show d-block';
+  }
+
+  return (
+    <div className={className} style={{ backgroundColor: '#000000b0' }}>
+      <div className="modal-dialog">
+        <div className="modal-content">
+          <div className="modal-header">
+            <h5 className="modal-title">{t('Delete Hoax')}</h5>
+          </div>
+          <div className="modal-body">{message}</div>
+          <div className="modal-footer">
+            <button className="btn btn-secondary" disabled={pendingApiCall} onClick={onClickCancel}>
+              {t('Cancel')}
+            </button>
+            <ButtonWithProgress
+              className="btn btn-danger"
+              onClick={onClickOk}
+              pendingApiCall={pendingApiCall}
+              disabled={pendingApiCall}
+              text={t('Delete Hoax')}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Modal;
