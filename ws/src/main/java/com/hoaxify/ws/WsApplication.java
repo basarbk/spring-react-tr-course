e008b1cf19f31189e925fb9bece6d9f521127789
Basar Buyukkahraman <basarbk@yahoo.com>
2020-10-10 02:00:00 +0300

    2108-app-config-changes

--- ws/src/main/java/com/hoaxify/ws/WsApplication.java
+++ ws/src/main/java/com/hoaxify/ws/WsApplication.java
@@ -22,16 +22,20 @@ public class WsApplication {
 	@Profile("dev")
 	CommandLineRunner createInitialUsers(UserService userService, HoaxService hoaxService) {
 		return (args) -> {
-			for(int i = 1; i<=25;i++) {				
-				User user = new User();
-				user.setUsername("user"+i);
-				user.setDisplayName("display"+i);
-				user.setPassword("P4ssword");
-				userService.save(user);
-				for(int j = 1;j<=20;j++) {
-					HoaxSubmitVM hoax = new HoaxSubmitVM();
-					hoax.setContent("hoax (" +j + ") from user ("+i+")");
-					hoaxService.save(hoax, user);
+			try {
+				userService.getByUsername("user1");				
+			} catch (Exception e) {				
+				for(int i = 1; i<=25;i++) {				
+					User user = new User();
+					user.setUsername("user"+i);
+					user.setDisplayName("display"+i);
+					user.setPassword("P4ssword");
+					userService.save(user);
+					for(int j = 1;j<=20;j++) {
+						HoaxSubmitVM hoax = new HoaxSubmitVM();
+						hoax.setContent("hoax (" +j + ") from user ("+i+")");
+						hoaxService.save(hoax, user);
+					}
 				}
 			}
 		};
